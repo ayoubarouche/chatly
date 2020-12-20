a201657c36613c170cae474074fd414a913c44d8
soultana-aicha <soultana.aicha99@gmail.com>
2020-12-21 00:48:10 +0100

    l'ajout des fichiers suivant : activity_main3.xml activity_main4.xml activity_main51.xml activity_main52.xml add_file_bg.xml cut_background.xml add_msg_bg.xml cut_background_add_devoir.xml ic_add_d.xml ic_add_dev.xml ic_add_file.xml ic_baseline_send_24.xml ic_chat.xml ic_cours.xml ic_delete.xml ic_devoir.xml ic_downward_dev.xml

---
+++ app/src/main/java/com/inpt/messagingapp/MainActivity52.java
@@ -0,0 +1,14 @@
+package com.inpt.messagingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity52 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main52);
+    }
+}
