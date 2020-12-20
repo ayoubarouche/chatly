6af8d92a9f06af4f715ca2560b27bbacb883a9bd
Ayoub Arouche <aarouche2016@gmail.com>
2020-12-20 20:33:05 +0100

    adding the firebase database configuration her i just configured the database firebase and architecture

--- app/src/main/java/com/inpt/messagingapp/MainActivity.java
+++ app/src/main/java/com/inpt/messagingapp/MainActivity.java
@@ -1,15 +1,27 @@
 package com.inpt.messagingapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.inpt.messagingapp.wrapper.controllers.teacher.TeacherCoursController;
+import com.inpt.messagingapp.wrapper.models.Cour;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button button = findViewById(R.id.hello_world);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TeacherCoursController tcontroller = new TeacherCoursController();
+                tcontroller.insertCours(new Cour("hello world","life is hard"));
+            }
+        });
     }
 }
--- app/src/main/java/com/inpt/messagingapp/wrapper/controllers/teacher/TeacherCoursController.java
+++ app/src/main/java/com/inpt/messagingapp/wrapper/controllers/teacher/TeacherCoursController.java
@@ -1,16 +1,55 @@
 package com.inpt.messagingapp.wrapper.controllers.teacher;
 
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.inpt.messagingapp.wrapper.controllers.CoursController;
 import com.inpt.messagingapp.wrapper.models.Cour;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TeacherCoursController implements CoursController {
+   static private FirebaseFirestore db;
+    static String TAG  = "hello world";
+    public TeacherCoursController(){
+        db= FirebaseFirestore.getInstance();
+
+    }
     @Override
     public List<Cour> getCourses(String uid) {
         return null;
     }
     public boolean DeleteCour(String coursId){
         return true;
+    }
+
+    //for test code hadi 4ir kantester la base de donnnes 5edama mezian hmd li lah
+
+    public void insertCours(Cour cour){
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Alan");
+        user.put("middle", "Mathison");
+        user.put("last", "Turing");
+        user.put("born", 1912);
+
+        db.collection("cours").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d(TAG ,"insert succes");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG ,"insert failed");
+            }
+        });
     }
 }
