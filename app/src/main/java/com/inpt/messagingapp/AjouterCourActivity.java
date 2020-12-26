2a7724ed03c092074dbed890ee898883ef6d460c
soultana-aicha <soultana.aicha99@gmail.com>
2020-12-26 13:41:48 +0100

    l'ajout des interfaces add cour et add devoir

---
+++ app/src/main/java/com/inpt/messagingapp/AjouterCourActivity.java
@@ -0,0 +1,14 @@
+package com.inpt.messagingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AjouterCourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter_cour);
+    }
+}
