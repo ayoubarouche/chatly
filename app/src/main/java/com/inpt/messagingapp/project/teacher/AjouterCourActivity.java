e2b36ceb8dd5c2fd5601adfeea1ad0ab1b607970
soultana-aicha <soultana.aicha99@gmail.com>
2021-01-02 22:39:50 +0100

    add cour

--- app/src/main/java/com/inpt/messagingapp/project/teacher/AjouterCourActivity.java
+++ app/src/main/java/com/inpt/messagingapp/project/teacher/AjouterCourActivity.java
@@ -1,6 +1,7 @@
 package com.inpt.messagingapp.project.teacher;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,12 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.inpt.messagingapp.GlobalApplication;
 import com.inpt.messagingapp.R;
 import com.inpt.messagingapp.helpers.sqliteHelpers.SqliteConnector;
@@ -20,7 +27,11 @@ import com.inpt.messagingapp.wrapper.models.Devoir;
 import com.inpt.messagingapp.wrapper.models.Message;
 import com.inpt.messagingapp.wrapper.models.User;
 
+import java.util.HashMap;
+
 public class AjouterCourActivity extends AppCompatActivity {
+    private static final int PICK_FILE = 1 ;
+    private DatabaseReference databaseReference;
     EditText title_input, description_input, file_input;
     Button add_button, cancel_button;
     Cour new_cour;
@@ -35,6 +46,7 @@ public class AjouterCourActivity extends AppCompatActivity {
         file_input = findViewById(R.id.addfile);
         add_button = findViewById(R.id.ajouter);
         cancel_button = findViewById(R.id.cancel);
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("UserOne");
         add_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -49,6 +61,11 @@ public class AjouterCourActivity extends AppCompatActivity {
 
             }
         });
+    }
+    public void FileUpload(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        startActivityForResult(intent,PICK_FILE);
     }
     public void addCour(Cour new_cour1){
         app.setTeacherCoursController();
@@ -71,6 +88,31 @@ public class AjouterCourActivity extends AppCompatActivity {
         intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
         startActivity(intent);
         this.finish();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_FILE){
+            if(resultCode == RESULT_OK){
+                Uri FileUri = data.getData();
+                StorageReference Folder = FirebaseStorage.getInstance().getReference().child("Files");
+                final StorageReference file_name = Folder.child("file"+FileUri.getLastPathSegment());
+                file_name.putFile(FileUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        file_name.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                HashMap<String,String> hashMap = new HashMap<>();
+                                hashMap.put("filelink", String.valueOf(uri));
+                                databaseReference.setValue(hashMap);
+                                Toast.makeText(AjouterCourActivity.this, "File Uploaded", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+            }
+        }
     }
 
 }
