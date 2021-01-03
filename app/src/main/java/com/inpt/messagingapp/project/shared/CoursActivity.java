7f91abd2f54e105bd04557a42bd3115fa2b07365
soultana-aicha <soultana.aicha99@gmail.com>
2021-01-03 16:59:38 +0100

    add menu

--- app/src/main/java/com/inpt/messagingapp/project/shared/CoursActivity.java
+++ app/src/main/java/com/inpt/messagingapp/project/shared/CoursActivity.java
@@ -3,11 +3,15 @@ package com.inpt.messagingapp.project.shared;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -31,11 +35,15 @@ public class CoursActivity extends AppCompatActivity {
     private GlobalApplication app ;
     private List<Cour> cours;
     private loadingDialog loading_dialog ;
+    private Toolbar toolbar;
+    private ImageView imageView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.consulter_cours_teacher);
         Button ajouter = findViewById(R.id.ajouter_button);
+        imageView = findViewById(R.id.imageView4);
+        registerForContextMenu(imageView);
         app = (GlobalApplication)getApplication();
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
@@ -77,6 +85,13 @@ public class CoursActivity extends AppCompatActivity {
         });
         loading_dialog.startLoadingDialog("chargement de votre cours ....");
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
     }
 
     public void getCoursesStudent(){
